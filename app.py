ad1ce6db6345c5819d44e8783ca45d083a7aeafd
Ian Webster <ianw_git@ianww.com>
2017-07-21 17:43:43 -0700

    initial commit

--- app.py
+++ app.py
@@ -1,56 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import urllib2
+import random
 import json
 
 from flask import Flask
 from flask import request
 from flask import jsonify
-from flask import render_template
-import os
-import random
-from utils import tag_from_text
+
+from quotes import QUOTES
+from character import CHARACTERS
 
 app = Flask(__name__)
 app.debug = True
 
-
-def get_dinopix_resp(text):
-    while True:
-        resp = urllib2.urlopen('http://dinosaurpictures.org/api/dinosaur/random').read()
-        obj = json.loads(resp)
-        name = obj['name']
-        period = obj['period']
-        eats = obj['eats']
-        regions = ', '.join(obj['regions'])
-        url = 'http://dinosaurpictures.org/%s-pictures' % name
-
-        if not period or not eats or not regions or not obj['shouldShowMap']:
-            continue
-
-        return {
-            'attachments': [
-                {
-                    'text': '*%s* lived in the %s and was a %s. It resided in %s.\n%s' \
-                            % (name, period, eats, regions, url),
-                    'image_url': obj['pics'][0]['url'],
-                },
-                {
-                    'image_url': obj['mapUrl'],
-                },
-            ],
-        }
+def get_resp(text):
+    quote = random.choice(QUOTES)
+    character = random.choice(CHARACTERS)
+    return {'text': '"%s" - %s' % (quote, character)}
 
 @app.route('/', methods=['GET', 'POST'])
-def return_dinopix_image():
+def index():
     if request.method == 'POST':
         text = request.form['text']
-        req = get_dinopix_resp(text)
+        req = get_resp(text)
         return jsonify(**req)
     elif request.method == 'GET':
-        return jsonify(get_dinopix_resp(''))
+        return jsonify(get_resp(''))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=4243)
---
+++ character.py
@@ -0,0 +1,13 @@
+CHARACTERS = [
+    'Spock',
+    'Captain Kirk',
+    'Leonard McCoy',
+    'Scotty',
+    'Klingon #12',
+    'George Takei',
+    'Jar-Jar Binks',
+    'USS Enterprise',
+    'Anakin from Star Trek',
+    'Jean-Luc Picard',
+    'Professor Snape',
+]
